26442514d4d62c4fc97b30bb26798d3de7e820c6
Aldebalan <tjdgus4093@naver.com>
2022-04-29 11:26:57 +0900

    예외처리

---
+++ chapter03/src/main/java/exception/ExceptionTest.java
@@ -0,0 +1,30 @@
+package exception;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10 - a;
+
+		System.out.println("some code.....1");
+
+		try {
+			System.out.println("some code.....2");
+
+			int result = (1 + 2 + 3) / 1;
+
+			System.out.println("some code......3");
+
+		} catch (ArithmeticException ex) {
+			// 예외 처리
+			// 1. 사과 
+			System.out.println("Sorry.");
+			// 2. 로깅 (logging) 기록
+			System.out.println("error:" + ex);
+			// 3. 정상종료
+			return;
+		} finally {
+			System.out.println("자원 정리");
+		}
+	}
+}
